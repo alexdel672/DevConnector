223b889ff4114d6419686069a7188edd1ddfa800
alexdelatorre <alexdel672@gmail.com>
2021-01-06 14:39:05 -0800

    added User model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,12 @@
 const express = require ('express');
 const router = express.Router();
 
-//@router   GET api/users 
-//@desc     Test route
+//@router   POST api/users 
+//@desc     register user
 //@access   Public
-router.get('/',(req,res) => res.send('User route'));
+router.post('/',(req,res) => {
+    console.log(req.body);
+    res.send('User route') 
+});
 
 module.exports = router;
